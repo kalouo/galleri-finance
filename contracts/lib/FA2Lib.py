a927855158c2edbf46f7bc9b825106cb219309d3
kalouo <omar@zipmex.com>
2022-07-03 17:07:11 +0200

    feat: set up tests with NFT balances

--- contracts/lib/FA2Lib.py
+++ contracts/lib/FA2Lib.py
@@ -19,14 +19,21 @@ class OwnableFA2NFT(FA2Lib.Admin,
         FA2Lib.Admin.__init__(self, admin)
 
 
-# class OwnableFA2Fungible(FA2Lib.Admin,
-#                          FA2Lib.ChangeMetadata,
-#                          FA2Lib.WithdrawMutez,
-#                          FA2Lib.OnchainviewBalanceOf,
-#                          FA2Lib.Fa2Fungible
-#                          ):
+class OwnableFA2Fungible(FA2Lib.Admin,
+                         FA2Lib.ChangeMetadata,
+                         FA2Lib.WithdrawMutez,
+                         FA2Lib.OnchainviewBalanceOf,
+                         FA2Lib.Fa2Fungible,
+                         FA2Lib.MintFungible
+                         ):
 
-#     def __init__(self, admin, metadata, token_metadata={}, ledger={}, policy=None, metadata_base=None):
-#         FA2Lib.Fa2Fungible.__init__(self, metadata, token_metadata=token_metadata,
-#                                ledger=ledger, policy=policy, metadata_base=metadata_base)
-#         FA2Lib.Admin.__init__(self, admin)
+    def __init__(self, admin, metadata, token_metadata={}, ledger={}, policy=None, metadata_base=None):
+        FA2Lib.Fa2Fungible.__init__(self, metadata, token_metadata=token_metadata,
+                                    ledger=ledger, policy=policy, metadata_base=metadata_base)
+        FA2Lib.Admin.__init__(self, admin)
+
+
+# Helpers
+
+def make_metadata(symbol, name, decimals):
+    return FA2Lib.make_metadata(symbol, name, decimals)
--- tests/loanCore_smartpy.py
+++ tests/loanCore_smartpy.py
@@ -3,6 +3,9 @@ import smartpy as sp
 LoanCore = sp.io.import_script_from_url("file:contracts/loanCore.py")
 LendingNote = sp.io.import_script_from_url("file:contracts/lendingNote.py")
 Constants = sp.io.import_script_from_url("file:contracts/lib/constants.py")
+FA2Lib = sp.io.import_script_from_url("file:contracts/lib/FA2Lib.py")
+
+SAMPLE_METADATA = sp.utils.metadata_of_url("http://example.com")
 
 
 @sp.add_test(name="A Test")
@@ -12,15 +15,59 @@ def test():
     # Initialize addresses.
     _admin = sp.test_account("_admin")
     _alice = sp.test_account("_alice")
+    _bob = sp.test_account("_bob")
 
     # Initialize contracts
+    nonFungibleToken = FA2Lib.OwnableFA2NFT(_admin.address, SAMPLE_METADATA)
+    fungibleToken = FA2Lib.OwnableFA2Fungible(_admin.address, SAMPLE_METADATA)
     loanCore = LoanCore.LoanCore(_admin.address)
-    lendingNote = LendingNote.LendingNote(
-        loanCore.address, Constants.SAMPLE_METADATA)
+    lendingNote = LendingNote.LendingNote(loanCore.address, SAMPLE_METADATA)
 
+    # Add contracts to scenarios
+    scenario += nonFungibleToken
+    scenario += fungibleToken
     scenario += loanCore
     scenario += lendingNote
 
+    TOKEN_0 = FA2Lib.make_metadata(
+        name="Example FA2",
+        decimals=0,
+        symbol="EFA2")
+
+    # Mint fungible tokens to Alice and Bob.
+    scenario += fungibleToken.mint([sp.record(
+        to_=_alice.address,
+        amount=100_000_000_000,
+        token=sp.variant("new", TOKEN_0)
+    ), sp.record(
+        to_=_bob.address,
+        amount=100_000_000_000,
+        token=sp.variant("existing", 0)
+    )]).run(sender=_admin)
+
+    # Verify that fungible tokens have been minted.
+    scenario.verify(fungibleToken.data.ledger[sp.pair(
+        _alice.address, 0)] == sp.nat(100_000_000_000))
+
+    scenario.verify(fungibleToken.data.ledger[sp.pair(
+        _bob.address, 0)] == sp.nat(100_000_000_000))
+
+    # Mint NFT to Bob.
+    NFT1 = FA2Lib.make_metadata(
+        name="Example FA2",
+        decimals=0,
+        symbol="EFA2-2")
+
+    scenario += nonFungibleToken.mint([
+        sp.record(
+            to_=_bob.address,
+            metadata=NFT1)
+    ]).run(sender=_admin)
+
+    # Verify that NFT was minted to Bob.
+    # Reflects NFT ledger type.
+    # See https://gitlab.com/tezos/tzip/-/blob/master/proposals/tzip-12/tzip-12.md#nft-asset-contract 
+    scenario.verify(nonFungibleToken.data.ledger[0] == _bob.address)
 
     # `lendingNote` is deployed with the correct administrator
     scenario.verify(lendingNote.data.administrator == loanCore.address)
