b3d81d3c5437f0a22d7e8b76455c106de60d56ba
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-11 13:20:57 +0200

    feat: loan_note origination

---
+++ originations/2-loan_note.py
@@ -0,0 +1,16 @@
+from chinstrap.originations import getContract
+
+
+def deploy(chinstrapState, network, accounts):
+    contract = getContract("loan_note")
+    initial_storage = contract.storage.encode(
+        {
+            "ledger": {},
+            "operators": {},
+            "token_metadata": {},
+            "last_token_id": 0,
+            "metadata": {'': '0x687474703a2f2f6578616d706c652e636f6d'},
+            "administrator": accounts[0].key.public_key_hash()
+        }
+    )
+    return initial_storage, contract
