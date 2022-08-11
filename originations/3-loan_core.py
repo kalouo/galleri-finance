748ca5c7b834b84b70b764df64d8baca50ef9945
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-11 13:57:15 +0200

    feat: originate loan_core

---
+++ originations/3-loan_core.py
@@ -0,0 +1,20 @@
+from chinstrap.originations import getContract
+
+
+def deploy(chinstrapState, network, accounts):
+    contract = getContract("loan_core")
+    initial_storage = contract.storage.encode(
+        {
+            "borrower_note_address": "tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU",
+            "collateral_vault_address": "tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU",
+            "lender_note_address": "tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU",
+            "interest_fee": 0,
+            "processing_fee": 0,
+            "loan_id": 0,
+            "currency_precision": {},
+            "loans_by_id": {},
+            "permitted_currencies": {},
+            "owner": accounts[0].key.public_key_hash()
+        }
+    )
+    return initial_storage, contract
