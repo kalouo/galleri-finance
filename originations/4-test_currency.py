e98c3f58a8adc06cbb70afff1eedfae02e8b9467
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-13 18:56:51 +0200

    originate sandbox tokens

---
+++ originations/4-test_currency.py
@@ -0,0 +1,17 @@
+from chinstrap.originations import getContract
+
+
+def deploy(chinstrapState, network, accounts):
+    contract = getContract("test_currency")
+    initial_storage = contract.storage.encode(
+        {
+            "administrator": accounts[0].key.public_key_hash(),
+            "last_token_id": 0,
+            "ledger": {},
+            "metadata": {'': '0x687474703a2f2f6578616d706c652e636f6d'},
+            "operators": {},
+            "supply": {},
+            "token_metadata": {},
+        }
+    )
+    return initial_storage, contract
