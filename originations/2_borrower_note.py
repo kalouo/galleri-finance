a470ff85cf30299bfa0ebc32b5333c498224458f
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-17 08:28:25 +0200

    rename origination files for ordering

--- originations/2_borrower_note.py
+++ originations/2_borrower_note.py
@@ -2,15 +2,17 @@ from chinstrap.originations import getContract
 
 
 def deploy(chinstrapState, network, accounts):
-    contract = getContract("lender_note")
+    contract = getContract("borrower_note")
     initial_storage = contract.storage.encode(
         {
             "ledger": {},
             "operators": {},
             "token_metadata": {},
-            "last_token_id": 0,
-            "metadata": {'': '0x687474703a2f2f6578616d706c652e636f6d'},
+            "last_token_id": 1,
+            "metadata": {'': '0x68474703a2f2f6578616d706c652e636f6d'},
             "administrator": accounts[0].key.public_key_hash()
         }
     )
     return initial_storage, contract
+
+
--- originations/3_lender_note.py
+++ originations/3_lender_note.py
@@ -2,15 +2,15 @@ from chinstrap.originations import getContract
 
 
 def deploy(chinstrapState, network, accounts):
-    contract = getContract("borrower_note")
+    contract = getContract("lender_note")
     initial_storage = contract.storage.encode(
         {
             "ledger": {},
             "operators": {},
             "token_metadata": {},
             "last_token_id": 0,
-            "metadata": {'': '0x687474703a2f2f6578616d706c652e636f6d'},
-            "administrator": accounts[0].key.public_key_hash()
+            "metadata": {'': '0x687474773a2f2f6578616d706c652e636f6d'},
+            "administrator": accounts[1].key.public_key_hash()
         }
     )
     return initial_storage, contract
